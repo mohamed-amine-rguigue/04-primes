20b18144ae18c6c320fe205ee8a41a3bb7c3867a
mohamed-amine.rguigue <mohamed-amine.rguigue@edu.esiee.fr>
2025-09-22 13:00:08 +0000

    Essai final

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+"""Connaitre les nombres premiers entre 0 et 100"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """Renvoie True si p est un nombre premier, False sinon"""
     if p<2:
-        return False    
+        return False
     for k in range (2, int(sqrt(p)+1)):
         if p%k==0:
             return False
@@ -16,6 +18,7 @@ def isprime(p):
 
 
 def main():
+    """Affiche les nombres premiers inférieurs à 100"""
 
     # vos appels à la fonction secondaire ici
 
